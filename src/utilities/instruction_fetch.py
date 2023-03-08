35d761c3d2d4705e1a6efac4712d903e6b824c21
Sumit Pravinkumar Patil <patilsumitpravinkumar@gmail.com>
2023-03-08 18:13:17 +0530

    some edits

--- src/utilities/instruction_fetch.py
+++ src/utilities/instruction_fetch.py
@@ -1,8 +1,8 @@
 import decode
 import single_cycle
 instruction_memory = {}
-pc = "0x0"
 next_pc = "0x0"
+pc = "0x0"
 
 
 def fetch():
@@ -11,5 +11,6 @@ def fetch():
     if(inst=='$'):
         single_cycle.end=1
     next_pc = str(hex(int(pc, 16) + 0x4))
-    decode.pc = pc
     decode.next_pc = next_pc
+    decode.pc = pc
+    
