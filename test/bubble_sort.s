c9994080854ef2173ad8ea8cae6bee3ab2e52aad
Karan <karanrajmehta06@gmail.com>
2023-03-08 16:08:59 +0530

    Updated test

--- test/bubble_sort.s
+++ test/bubble_sort.s
@@ -2,7 +2,8 @@
 # Sorts an array of random numbers using bubble sort, and stores the sorted array
 jal zero, main
 
-random:    # Generates a pseudo random number using a seed value
+random:
+    # Generates a pseudo random number using a seed value
     add t3, zero, tp    # t3 = tp = seed
     slli t4, t3, 21    # t4 = t3 << 21
     xor t3, t3, t4    # t3 ^= t4
@@ -11,14 +12,17 @@ random:    # Generates a pseudo random number using a seed value
     xor t3, t3, t4    # t3 ^= t4
     add tp, zero, t3    # tp = seed = t3
     add a0, zero, t3    # a0 = t3
-    ret    # jalr zero, 0(ra)    # return
+    jalr zero, 0(ra)    # return
 
-bubbleSort:    # Implements the bubble sort algorithm
+bubbleSort:
+    # Implements the bubble sort algorithm
     add t0, zero, zero    # to = loop2 counter = 0
-    loop2:    # Start of loop2
+    # Start of loop2
+    loop2:
         beq t0, gp, end2    # if t0 == gp, goto end2
         add t1, zero, zero    # t1 = loop3 counter = 0
-        loop3:    # Start of loop3
+        # Start of loop3
+        loop3:
             addi t2, t0, 1    # t2 = t0 + 1
             sub t2, gp, t2    # t2 = gp - t2
             beq t1, t2, end3    # if t1 == t2, goto loop2
@@ -35,14 +39,18 @@ bubbleSort:    # Implements the bubble sort algorithm
             # Swap
             sw t3, 0(t4)    # Store t3 in the address pointed to by t4
             sw t5, 0(t2)    # Store t5 in the address pointed to by t2
-            label:    # Continue to label
+
+            # Continue to label
+            label:
                 addi t1, t1, 1    # t1++
                 beq zero, zero, loop3    # Continue to loop3
+
         end3:
             addi t0, t0, 1    # t0++
             beq zero, zero, loop2
+
     end2:
-    ret    # jalr zero, 0(ra)    # return
+    jalr zero, 0(ra)    # return
 
 main:
     addi gp, zero, 10    # gp = N = 10
@@ -53,6 +61,7 @@ main:
     add t0, zero, zero    # t0 = loop1 counter = 0
     add t1, zero, s0    # t1 = s0
     add t2, zero, s1    # t2 = s1
+
     loop1:
         beq t0, gp, end1    # if t0 == gp, goto end1
         jal ra, random    # Call random
@@ -66,5 +75,6 @@ main:
         addi t1, t1, 4
         addi t2, t2, 4
         beq zero, zero, loop1
+
     end1:
     jal ra, bubbleSort
--- test/fibonacci.s
+++ test/fibonacci.s
@@ -1,13 +1,16 @@
-add x1, x0, x0 # x1 = x0 + x0
-addi x2, x0, 1 # x2 = x0 + 1
-addi x3, x0, 10 # x3 = x0 + n
-addi x4, x0, 2 # x4 = x0 + 2
-bne x3, x0, loop # if x3 != x0 then next
-add x2, x1, x0 # x2 = x1 + x0
-loop: bgt x4, x3, exit # if x4 > x3 then exit
-add x5, x1, x2 # x5 = x1 + x2
-add x1, x2, x0 # x1 = x2 + x0
-add x2, x5, x0 # x2 = x5 + x0
-addi x4, x4, 1 # x4 = x4 + 1
-beq x0, x0, loop # if x0 == x0 then loop
+add x1, x0, x0    # x1 = 0
+addi x2, x0, 1    # x2 = 1
+addi x3, x0, 10    # x3 = n
+addi x4, x0, 2    # x4 = 2
+bne x3, x0, loop    # if x3 != 0, goto next
+add x2, x1, x0    # x2 = x1
+
+loop:
+    bgt x4, x3, exit    # if x4 > x3, goto exit
+    add x5, x1, x2    # x5 = x1 + x2
+    add x1, x2, x0    # x1 = x2
+    add x2, x5, x0    # x2 = x5
+    addi x4, x4, 1    # x4++
+    beq x0, x0, loop    # goto loop
+
 exit:
