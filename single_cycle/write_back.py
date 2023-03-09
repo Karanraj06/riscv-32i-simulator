f80956bde4aa1324a8e61daa6827870f5e4ac0cd
Atharva Suhas Mulay <100485953+AtharvaMulay25@users.noreply.github.com>
2023-03-10 00:50:37 +0530

    updated write_back.py

---
+++ single_cycle/write_back.py
@@ -0,0 +1,41 @@
+import instruction_fetch as fi
+import decode as de
+import execute as ex
+import memory_access as ma
+import registers as rg
+
+# Initialize clock to 0
+clk: int = 0
+
+
+def writeBack() -> None:
+    """Writeback the result into the Register File by selecting ResultSelect accordingly."""
+
+    # if RFWrite is 0, then there is no need to write result
+    if de.RFWrite == 0:
+        clk += 1
+        return
+    # if rd is set to x0, then its value can't be updated
+    if int(de.rd,2) is not 0:
+        # Selecting ResultSelect
+        if de.ResultSelect == 0:
+            rg.x[int(de.rd, 2)] = ex.aluResult
+        elif de.ResultSelect == 1:
+            rg.x[int(de.rd, 2)] = ma.loadData
+        elif de.ResultSelect == 2:
+            rg.x[int(de.rd, 2)] = de.immU
+        elif de.ResultSelect == 3:
+            rg.x[int(de.rd, 2)] = fi.pc + 4
+        elif de.ResultSelect == 4:
+            rg.x[int(de.rd, 2)] = de.immU + fi.pc
+        with open("output.txt", "a") as f:
+            f.write(f"WRITEBACK: write {rg.x[int(de.rd,2)]} to rd = x[{int(de.rd, 2)}]")
+    # incrementing clock cycle by 1 after the writeback stage is completed
+    clk += 1
+
+
+def init() -> None:
+    """Initializes pc to its initial value"""
+    global clk
+    # initialising clk cycle to 0
+    clk = 0
