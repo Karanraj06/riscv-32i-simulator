05c3431465499fa99e23446de385277122893cb9
Sumit Pravinkumar Patil <patilsumitpravinkumar@gmail.com>
2023-03-09 22:35:26 +0530

    Update Memory Access

---
+++ single_cycle/memory_access.py
@@ -0,0 +1,25 @@
+import decode as de
+import execute as ex
+from collections import defaultdict
+#Data Memory
+data_memory:defaultdict[int,str]=defaultdict(lambda:None)
+
+def dec_to_bin(x:int)->str:
+    ret=bin(x)
+    if(x<0):
+        ret=ret[2:].rjust(32,'1')
+    else:
+        ret=ret[2:].zfill(32)    
+
+def memory_access()->None:
+    if(de.MemOp==0):
+        pass
+    elif(de.MemOp==1):
+        loadData=de.bin_to_dec(data_memory[ex.aluResult])
+    elif(de.MemOp==2):
+        data_memory[ex.aluResult]=dec_to_bin(de.op2)
+
+def update_mem(address:str,value:str)->None:
+    global data_memory
+    data_memory[int(address,16)]=bin(int(value, 16))[2:].zfill(32)
+
