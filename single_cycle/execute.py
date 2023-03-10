6fef741584b1b4a8b3e7b5d387d648390d0eb6de
nishad-dhuri-05 <ndhuri24@gmail.com>
2023-03-10 20:15:16 +0530

    print execute

--- single_cycle/execute.py
+++ single_cycle/execute.py
@@ -26,63 +26,63 @@ def execute() -> int:
     f = open("output.txt", "a")
     if (de.ALUOperation == 0):
         aluResult = op1+op2
-        f.write(f"EXECUTE:ADD {op1} and {op2}")
+        f.write(f"EXECUTE:ADD {op1} and {op2}\n")
     elif (de.ALUOperation == 1):
         aluResult = op1-op2
-        f.write(f"EXECUTE:SUB {op1} and {op2}")
+        f.write(f"EXECUTE:SUB {op1} and {op2}\n")
     elif (de.ALUOperation == 2):
         aluResult = op1 ^ op2
-        f.write(f"EXECUTE:XOR {op1} and {op2}")
+        f.write(f"EXECUTE:XOR {op1} and {op2}\n")
     elif (de.ALUOperation == 3):
         aluResult = op1 | op2
-        f.write(f"EXECUTE:OR {op1} and {op2}")
+        f.write(f"EXECUTE:OR {op1} and {op2}\n")
     elif (de.ALUOperation == 4):
         aluResult = op1 & op2
-        f.write(f"EXECUTE:AND {op1} and {op2}")
+        f.write(f"EXECUTE:AND {op1} and {op2}\n")
     elif (de.ALUOperation == 5):
         aluResult = op1 << op2
-        f.write(f"EXECUTE:SLL {op1} and {op2}")
+        f.write(f"EXECUTE:SLL {op1} and {op2}\n")
     elif (de.ALUOperation == 6):
         aluResult=srl(op1,op2)
-        f.write(f"EXECUTE:SRL {op1} and {op2}")
+        f.write(f"EXECUTE:SRL {op1} and {op2}\n")
     elif (de.ALUOperation == 7):
         aluResult = op1 >> op2
-        f.write(f"EXECUTE:SRA {op1} and {op2}")
+        f.write(f"EXECUTE:SRA {op1} and {op2}\n")
     elif (de.ALUOperation == 8):
         if (op1 == op2):
             isBranch = 1
             fi.pc = de.BranchTargetAddress
-            f.write(f"EXECUTE:BEQ PC set to {de.BranchTargetAddress}")
+            f.write(f"EXECUTE:BEQ PC set to {de.BranchTargetAddress}\n")
         else:
             isBranch = 0
     elif (de.ALUOperation == 9):
         if (op1 != op2):
             isBranch = 1
             fi.pc = de.BranchTargetAddress
-            f.write(f"EXECUTE:BNE PC set to {de.BranchTargetAddress}")
+            f.write(f"EXECUTE:BNE PC set to {de.BranchTargetAddress}\n")
         else:
             isBranch = 0
     elif (de.ALUOperation == 10):
         if (op1 >= op2):
             isBranch = 1
             fi.pc = de.BranchTargetAddress
-            f.write(f"EXECUTE:BGE PC set to {de.BranchTargetAddress}")
+            f.write(f"EXECUTE:BGE PC set to {de.BranchTargetAddress}\n")
         else:
             isBranch = 0
     elif (de.ALUOperation == 11):
         if (op1 < op2):
             isBranch = 1
             fi.pc = de.BranchTargetAddress
-            f.write(f"EXECUTE:BLT PC set to {de.BranchTargetAddress}")
+            f.write(f"EXECUTE:BLT PC set to {de.BranchTargetAddress}\n")
         else:
             isBranch = 0
     # for JAL
     if (de.opcode == '1101111'):
         fi.pc = de.BranchTargetAddress
-        f.write(f"EXECUTE: PC set to {de.BranchTargetAddress}")
+        f.write(f"EXECUTE: PC set to {de.BranchTargetAddress}\n")
     elif (de.opcode == '1100111'):  # for JALR
         fi.pc = aluResult
-        f.write(f"EXECUTE: PC set to {de.BranchTargetAddress}")
+        f.write(f"EXECUTE: PC set to {de.BranchTargetAddress}\n")
     f.close()
     print(aluResult)
 
